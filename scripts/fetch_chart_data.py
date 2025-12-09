d51a38f964274490d987a551ca7fd8576975f4c6
MartinInsight <martin@insightshippinginc.com>
2025-12-09 13:14:51 -0800

    Update fetch_chart_data.py

--- scripts/fetch_chart_data.py
+++ scripts/fetch_chart_data.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import numpy as np
 import sys
 
-# 현재 스크립트의 디렉토리를 sys.path에 추가하여 로컬 모듈을 찾을 수 있도록 함.
+# 현재 스크립트의 디렉토리를 sys.path에 추가하여 로컬 모듈을 찾을 수 있도록 함
 script_dir = os.path.dirname(__file__)
 if script_dir not in sys.path:
     sys.path.append(script_dir)
